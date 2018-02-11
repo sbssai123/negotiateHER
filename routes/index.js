fed421c3c1f20d3a008d6c95ecbc4597347b6cc7
sbssai123 <sbs.sai@gmail.com>
2018-02-11 05:00:00 -0500

    playing with d3

--- routes/index.js
+++ routes/index.js
@@ -10,3 +10,15 @@ module.exports = function (router) {
         res.sendFile(path.join(__dirname + '/../src/views/Sample.html'));
     });
 }
+
+
+
+var data = [30, 86, 168, 281, 303, 365];
+
+d3.select(".chart")
+  .selectAll("div")
+  .data(data)
+    .enter()
+    .append("div")
+    .style("width", function(d) { return d + 'px' })
+    .text(function(d) { return '$ ' + d; });
